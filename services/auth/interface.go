1d66d5894ec1715d70187ce50b4f7302da07b8a7
Richard Alison <richardalison86@gmail.com>
2024-05-10 05:38:05 +0900

    chore(auth): remove unused mock generator

--- services/auth/interface.go
+++ services/auth/interface.go
@@ -1,7 +1,5 @@
 package auth
 
-//go:generate mockgen -destination mock_test.go -package auth_test . AuthService
-
 type AuthService interface {
 	RegisterStaff(req RegisterStaffReq, res *RegisterStaffRes) error
 	LoginStaff(req LoginStaffReq, res *LoginStaffRes) error
