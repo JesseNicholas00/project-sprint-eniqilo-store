f9fc791faffda34833ae8693a49f776b7d8d9add
Marcel <marcelsprd2000@gmail.com>
2024-05-09 20:23:50 +0700

    Change return from pointer in Repo

--- repos/product/delete_product.go
+++ repos/product/delete_product.go
@@ -9,26 +9,26 @@ var deleteProductRepoLogger = logging.GetLogger(
 	"deleteProduct",
 )
 
-func (repo *productRepositoryImpl) DeleteProduct(id string) (*ProductDeleteResult, error) {
+func (repo *productRepositoryImpl) DeleteProduct(id string) (ProductDeleteResult, error) {
 	deleteQuery := `DELETE FROM products WHERE product_id = $1`
 
 	results, err := repo.db.Exec(deleteQuery, id)
+	res := ProductDeleteResult{}
 	if err != nil {
 		deleteProductRepoLogger.Printf(
 			"error while deleteProduct() caused by: %s",
 			err,
 		)
-		return nil, err
+		return res, err
 	}
 
 	rowsAffected, err := results.RowsAffected()
 	if err != nil {
-		return nil, err
+		return res, err
 	}
 
-	res := ProductDeleteResult{
-		Message:      "success",
-		RowsAffected: int(rowsAffected),
-	}
-	return &res, nil
+	res.RowsAffected = int(rowsAffected)
+	res.Message = "success"
+
+	return res, nil
 }
